ed76e71eecbd294b76e7f1f9d536af807ccf628b
JSasa <salegavric@outlook.com>
2018-02-10 01:20:08 +0100

    Add error landing page

--- W.e-library/src/com/sasagavric/spring/aop/EmailAspect.java
+++ W.e-library/src/com/sasagavric/spring/aop/EmailAspect.java
@@ -3,7 +3,6 @@ package com.sasagavric.spring.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,11 +11,8 @@ public class EmailAspect {
 	
 	@AfterThrowing(pointcut = "execution( * com.sasagavric.spring.*.*.*(..) )", throwing="exc")
 	public void adviceForEverything(JoinPoint theJoinPoint, Throwable exc) {
-		System.out.println("@Before");
 		
-		MethodSignature theMethodSignathure = (MethodSignature) theJoinPoint.getSignature();
-		
-		System.out.println("Metoda" + theMethodSignathure + "/Shit happen" + exc.getMessage());
+		//coming soon...
 		
 	}
 
